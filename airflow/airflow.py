4c3c7ed18bbd790eeb35e6a4f8d0e0e1c540ec9a
wylliams <76174006+Kozato01@users.noreply.github.com>
2023-08-25 12:10:28 -0300

    Update airflow.py

--- airflow/airflow.py
+++ airflow/airflow.py
@@ -4,15 +4,15 @@ from datetime import datetime
 
 default_args = {
     'owner': 'padrao',
-    'start_date': datetime(2023, 8, 24),  # Defina a data de início desejada
+    'start_date': datetime(2023, 8, 24),
     'retries': 1,
 }
 
 dag = DAG(
     'meu_workflow',
     default_args=default_args,
-    schedule_interval=None,  # Defina a frequência de execução (ou None para executar manualmente)
-    catchup=False  # Defina como False se não quiser recuperar tarefas antigas
+    schedule_interval=None,
+    catchup=False  
 )
 
 def execute_py_file(file_name):
@@ -22,24 +22,23 @@ def execute_py_file(file_name):
 bronze_task = PythonOperator(
     task_id='bronze_task',
     python_callable=execute_py_file,
-    op_args=['etl\raw.py'],  # Substitua pelo caminho correto para bronze.py
+    op_args=['etl\raw.py'], 
     dag=dag
 )
 
 silver_task = PythonOperator(
     task_id='silver_task',
     python_callable=execute_py_file,
-    op_args=['etl\silver.py'],  # Substitua pelo caminho correto para silver.py
+    op_args=['etl\silver.py'], 
     dag=dag
 )
 
 gold_task = PythonOperator(
     task_id='gold_task',
     python_callable=execute_py_file,
-    op_args=['etl\gold.py'],  # Substitua pelo caminho correto para gold.py
+    op_args=['etl\gold.py'], 
     dag=dag
 )
-
 
 # Defina a ordem das tarefas
 bronze_task >> silver_task >> gold_task
