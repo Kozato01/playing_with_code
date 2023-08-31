45776087db7c0b8ea1c2cece5ef2eb811779db64
wylliams <76174006+Kozato01@users.noreply.github.com>
2023-08-31 14:06:54 -0300

    Update strem.py

--- count/strem.py
+++ count/strem.py
@@ -1,16 +1,26 @@
 import streamlit as st
-@st.cache_data
-def initialize_access_count():
-    return 0
 
-# Função para atualizar o contador de acessos
+# Função para ler ou inicializar o contador de acessos
+def get_access_count():
+    try:
+        with open('access_count.txt', 'r') as f:
+            return int(f.read())
+    except FileNotFoundError:
+        return 0
+
+# Função para atualizar e exibir o contador de acessos
 def update_access_count():
-    access_count = initialize_access_count() + 1
-    return access_count
+    count = get_access_count() + 1
+    with open('access_count.txt', 'w') as f:
+        f.write(str(count))
+    return count
 
-
-st.title("Contador de Acesso")
+def main():
+    st.title("Contador de Acesso")
 
     # Atualiza o contador quando a página é acessada
-access_count = update_access_count()
-st.write(f"Esta página foi acessada {access_count} vezes.")
+    access_count = update_access_count()
+    st.write(f"Esta página foi acessada {access_count} vezes.")
+
+if __name__ == '__main__':
+    main()
