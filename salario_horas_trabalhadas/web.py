ba49c9e25edd88f92ce25ec09a60bdfab2a30d7b
Ana Coelho <70607969+anabcoelho@users.noreply.github.com>
2023-09-30 14:12:05 -0300

    Update web.py

--- salario_horas_trabalhadas/web.py
+++ salario_horas_trabalhadas/web.py
@@ -81,12 +81,12 @@ def main():
     ano = st.number_input("Digite o ano:", min_value=1900, max_value=2100, value=datetime.datetime.today().year)
     metodo_calculo = st.selectbox("Escolha o tipo:", ["Por Hora", "Valor Fixo"])
     
+    valor_fixo, taxa_horaria = 0, 0
     if metodo_calculo == "Por Hora":
         taxa_horaria = st.number_input("Digite sua taxa horária:", min_value=0.0, value=23.0)
-        valor_fixo = 0
     else:
         valor_fixo = st.number_input("Digite o valor fixo:", min_value=0.0, value=1500.0)
-        taxa_horaria = 0
+        
 
     horas_trabalho_mes, num_dias_uteis = calcular_horas_trabalho_mes(mes, ano)
     if horas_trabalho_mes == 0:
@@ -94,14 +94,12 @@ def main():
         return
         
     imposto_escolha = st.checkbox("Incluir imposto?", value=False)
-    porcentagem = st.number_input("Digite a porcentagem do imposto:", min_value=0, max_value=100, value=6) if imposto_escolha else 0
-
     DAS_escolha = st.checkbox("Incluir DAS?", value=False)
+    porcentagem = st.number_input("Digite a porcentagem do imposto:", min_value=0, max_value=100, value=6) if imposto_escolha else 0
     valor_das = 71 if DAS_escolha else 0
     #st.number_input("Valor do DAS:", min_value=0, value=71.0) 
 
     salario_mensal = calcular_salario_com_valor_fixo(horas_trabalho_mes, taxa_horaria, valor_fixo, metodo_calculo)
-
     imposto_calculado = (salario_mensal * (porcentagem/100)) + valor_das 
     salario_liquido = salario_mensal - imposto_calculado
 
@@ -114,17 +112,12 @@ def main():
         st.write("Valor do imposto: R$", imposto_calculado)
         st.write("Salário mensal liquido estimado: R$", salario_liquido)
         salario_final = salario_liquido
-
-# indicações pra galera
-    despesas_basicas = salario_final * 0.60
-    poupanca_investimento = salario_final * 0.20
-    gastos_pessoais = salario_final * 0.20
-    
+   
     st.write('-'*15)
     st.write('Recomendação de divisão do salário.')
-    st.write(f'Despesas Básicas: R$ {despesas_basicas:.2f}')
-    st.write(f'Poupança e Investimentos: R$ {poupanca_investimento:.2f}')
-    st.write(f'Gastos Pessoais: R$ {gastos_pessoais:.2f}')
+    st.write(f'Despesas Básicas: R$ {(salario_final * 0.60):.2f}')
+    st.write(f'Poupança e Investimentos: R$ {(salario_final * 0.20):.2f}')
+    st.write(f'Gastos Pessoais: R$ {(salario_final * 0.20):.2f}')
 
     add_linked_in_icon()
     
