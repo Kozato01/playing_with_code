c8b5e501244bb1010c37f3bf7cc6cbf0bfb34a89
Ana Coelho <70607969+anabcoelho@users.noreply.github.com>
2023-09-30 13:00:12 -0300

    Update web.py

--- salario_horas_trabalhadas/web.py
+++ salario_horas_trabalhadas/web.py
@@ -85,9 +85,9 @@ def main():
         porcentagem = st.number_input("Digite a porcentagem do imposto:", min_value=0, max_value=100, value=6)
 
     DAS_escolha = st.checkbox("Incluir DAS?", value=False)
-    valor_das = 0
+    #valor_das = 0
     if DAS_escolha:
-        valor_das = st.number_input("Valor do DAS:", min_value=0, value=71.0)
+        valor_das = 71 #st.number_input("Valor do DAS:", min_value=0, value=71.0)
 
     salario_mensal = calcular_salario_com_valor_fixo(horas_trabalho_mes, taxa_horaria, valor_fixo, metodo_calculo)
 
